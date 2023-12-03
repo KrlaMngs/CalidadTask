99a9e6b4f2280b6be3e475c3b78cf77075c2fade
KrlaMngs <150064105+KrlaMngs@users.noreply.github.com>
2023-12-02 21:37:59 -0500

    Update import pandas as pd2.py

--- import pandas as pd2.py
+++ import pandas as pd2.py
@@ -4,11 +4,18 @@
 #Objetivo General
 # Crear resultados estadisticos para análizar las métricas de calidad del presente archivo
 
+#Fuente de Información
+#https://www.kaggle.com/datasets/sayedmohsin/sqa-dataset
+
+#Descripción de las variables a medir alineadas con las categorías, subcategorías y métricas según ISO 25000
+
+
+#Importamos las librerias
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Cargar el archivo CSV
+# Cargamos el archivo CSV con los datos
 file_path = "C:/Users/karla/Downloads/archive/dataset/resulted/class_level_quality_code_smell_mdi.csv"
 df = pd.read_csv(file_path)
 
@@ -55,3 +62,9 @@ sns.boxplot(x='filename', y='Complexity', data=df)
 plt.xticks(rotation=90)
 plt.title('Comparación de Complejidad entre Archivos')
 plt.show()
+
+
+#Conclusiones y Recomendaciones
+#La distribución de la complejidad en el código muestra que la mayoría de las clases tienen una complejidad baja, pero hay algunas clases con complejidad significativamente más alta.
+#El boxplot del acoplamiento revela que existen algunas clases con niveles de acoplamiento más altos que la mayoría. Esto podría indicar áreas de mejora en términos de desacoplamiento.
+#El diagrama de dispersión entre CBO y RFC muestra si hay una relación entre la complejidad y la cantidad de métodos llamados. Puede ser útil para identificar clases con posibles problemas de diseño.
